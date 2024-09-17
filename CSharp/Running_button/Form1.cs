0782557e189a953ed93e814da08e8be3bd54b2e3
Kyrylo Hurieiev <k.huryeyev_fit_3_23_b_d@knute.edu.ua>
2024-09-17 11:41:44 +0300

    Clear

---
+++ CSharp/Running_button/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Running_button
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int buttonX = button2.Location.X;
+            int buttonY = button2.Location.Y;
+
+            if (Math.Abs(e.X - (buttonX + button2.Width / 2)) < 100 && Math.Abs(e.Y - (buttonY + button2.Height / 2)) < 50)
+            {
+                int newX = random.Next(0, this.ClientSize.Width - button2.Width);
+                int newY = random.Next(0, this.ClientSize.Height - button2.Height);
+                button2.Location = new Point(newX, newY);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Оце впертість...Все ж натиснули");
+        }
+    }
+}
---
+++ CSharp/Running_button/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Running_button
+{
+
+    public partial class Form2 : Form
+    {
+        private Random random = new Random();
+        public Form2()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int buttonX = button1.Location.X;
+            int buttonY = button1.Location.Y;
+
+            if (Math.Abs(e.X - (buttonX + button1.Width / 2)) < 100 && Math.Abs(e.Y - (buttonY + button1.Height / 2)) < 50)
+            {
+                int newX = random.Next(0, this.ClientSize.Width - button1.Width);
+                int newY = random.Next(0, this.ClientSize.Height - button1.Height);
+                button1.Location = new Point(newX, newY);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Оце впертість...Все ж натиснули");
+        }
+    }
+}

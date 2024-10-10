491db1ed219827288c92f670b7af15659cebdc41
Kyrylo Hurieiev <k.huryeyev_fit_3_23_b_d@knute.edu.ua>
2024-10-10 12:37:04 +0300

    Lab6.Beta

--- CSharp/lab6/Models/User.cs
+++ CSharp/lab6/Models/User.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; } // Ім'я
         public string LastName { get; set; } // Прізвище
         public string Email { get; set; } // Електронна адреса
+        public string PhoneNumber { get; set; }
         public DateTime RegistrationDate { get; set; } // Дата реєстрації
     }
 }
